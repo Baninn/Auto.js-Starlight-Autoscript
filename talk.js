7f149431dfbd7011661dc7538b95028f6edb9002
Baninn <bi.yibo@163.com>
2019-09-09 18:34:32 +0900

    加入随机坐标点击

--- talk.js
+++ talk.js
@@ -1,51 +1,43 @@
+auto();
+requestScreenCapture();
 /*
 do{
     log("找home");
-    auto();
-    requestScreenCapture();
-    var home = images.read("/storage/emulated/0/img/home.jpg");
-    var home = findImage(captureScreen(), home, {threshold: 0.8});
+    var home_pic = images.read("/storage/emulated/0/img/home.jpg");
+    var home = findImage(captureScreen(), home_pic, {threshold: 0.8});
     if(home){
         log("找到home");
-        let { x, y } = home ;
-        click(x, y);
+        home.x += Math.round(Math.random() * home_pic.getWidth());
+        home.y += Math.round(Math.random() * home_pic.getHeight());
+        click(home.x, home.y);
     }
   }
 while(home==null);
 sleep(1000);
 */
-/**
- * talk
- * voice
- * menu
- * skip
- * skip2
- * ok
- */
+
 do{
     log("找talk");
-    auto();
-    requestScreenCapture();
-    var talk = images.read("/storage/emulated/0/img/talk.jpg");
-    var talk = findImage(captureScreen(), talk, {threshold: 0.8});
+    var talk_pic = images.read("/storage/emulated/0/img/talk.jpg");
+    var talk = findImage(captureScreen(), talk_pic, {threshold: 0.8});
     if(talk){
         log("找到talk");
-        let { x, y } = talk ;
-        click(x, y);
+        talk.x += Math.round(Math.random() * talk_pic.getWidth());
+        talk.y += Math.round(Math.random() * talk_pic.getHeight());
+        click(talk.x, talk.y);
     }
   }
 while(talk==null);
 sleep(1000);
 do{
     log("找voice");
-    auto();
-    requestScreenCapture();
-    var voice = images.read("/storage/emulated/0/img/voice.jpg");
-    var voice = findImage(captureScreen(), voice, {threshold: 0.8});
+    var voice_pic = images.read("/storage/emulated/0/img/voice.jpg");
+    var voice = findImage(captureScreen(), voice_pic, {threshold: 0.8});
     if(voice){
         log("找到voice");
-        let { x, y } = voice ;
-        click(x, y);
+        voice.x += Math.round(Math.random() * voice_pic.getWidth());
+        voice.y += Math.round(Math.random() * voice_pic.getHeight());
+        click(voice.x, voice.y);
     }
   }
 while(voice==null);
@@ -55,12 +47,13 @@ do{
     log("找menu");
     auto();
     requestScreenCapture();
-    var menu = images.read("/storage/emulated/0/img/menu.jpg");
-    var menu = findImage(captureScreen(), menu, {threshold: 0.8});
+    var menu_pic = images.read("/storage/emulated/0/img/menu.jpg");
+    var menu = findImage(captureScreen(), menu_pic, {threshold: 0.8});
     if(menu){
         log("找到menu"+menu);
-        let { x, y } = menu ;
-        click(x, y);
+        menu.x += Math.round(Math.random() * menu_pic.getWidth());
+        menu.y += Math.round(Math.random() * menu_pic.getHeight());
+        click(menu.x, menu.y);
     }
   }
 while(menu==null);
@@ -69,12 +62,13 @@ do{
     log("找skip");
     auto();
     requestScreenCapture();
-    var skip = images.read("/storage/emulated/0/img/skip.jpg");
-    var skip = findImage(captureScreen(), skip, {threshold: 0.8});
+    var skip_pic = images.read("/storage/emulated/0/img/skip.jpg");
+    var skip = findImage(captureScreen(), skip_pic, {threshold: 0.8});
     if(skip){
         log("找到skip"+skip);
-        let { x, y } = skip ;
-        click(x, y);
+        skip.x += Math.round(Math.random() * skip_pic.getWidth());
+        skip.y += Math.round(Math.random() * skip_pic.getHeight());
+        click(skip.x, skip.y);
     }
   }
 while(skip==null);
@@ -83,12 +77,13 @@ do{
     log("找skip2");
     auto();
     requestScreenCapture();
-    var skip2 = images.read("/storage/emulated/0/img/skip2.jpg");
-    var skip2 = findImage(captureScreen(), skip2, {threshold: 0.8});
+    var skip2_pic = images.read("/storage/emulated/0/img/skip2.jpg");
+    var skip2 = findImage(captureScreen(), skip2_pic, {threshold: 0.8});
     if(skip2){
         log("找到skip2"+skip2);
-        let { x, y } = skip2 ;
-        click(x, y);
+        skip2.x += Math.round(Math.random() * skip2_pic.getWidth());
+        skip2.y += Math.round(Math.random() * skip2_pic.getHeight());
+        click(skip2.x, skip2.y);
     }
   }
 while(skip2==null);
@@ -97,12 +92,13 @@ do{
     log("找ok");
     auto();
     requestScreenCapture();
-    var ok = images.read("/storage/emulated/0/img/ok.jpg");
-    var ok = findImage(captureScreen(), ok, {threshold: 0.8});
+    var ok_pic = images.read("/storage/emulated/0/img/ok.jpg");
+    var ok = findImage(captureScreen(), ok_pic, {threshold: 0.8});
     if(ok){
       log("找到ok"+ok);
-      let { x, y } = ok ;
-      click(x, y);
+      ok.x += Math.round(Math.random() * ok_pic.getWidth());
+      ok.y += Math.round(Math.random() * ok_pic.getHeight());
+      click(ok.x, ok.y);
   }
 }
 while(ok==null);
